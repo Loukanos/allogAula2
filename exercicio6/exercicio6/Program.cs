21b35e39b24b1f69a23cefa0a914781b5aca7718
Lucas <62773051+Loukanos@users.noreply.github.com>
2023-05-08 17:28:04 -0300

    Add files via upload
    Exercício 6: Lucas

---
+++ exercicio6/exercicio6/Program.cs
@@ -0,0 +1,34 @@
+﻿//Lucas
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        bool temA = false;
+        Console.WriteLine("Entre com a frase:");
+        string input = Console.ReadLine();
+        char[] chars = input.ToCharArray();//cria um array de caracteres através do string
+
+        for (int i = 0; i < chars.Length; i++)//percorre array e realiza as substituições necessárias
+        {
+            if (chars[i] == 'A' || chars[i] == 'a')
+            {
+                temA = true;
+                chars[i] = '&';
+            }
+        }
+
+        if (temA==true)//só checa se a frase realmente seria modificada
+        {
+            Console.WriteLine("Frase modificada:");
+            input = new string(chars);//transfere os caracteres modificados de volta ao input
+            Console.WriteLine(input);
+        }
+        else//se não for, avisa usuário
+        {
+            Console.Clear();//só pra bonito
+            Console.WriteLine("Sua frase não tem 'A' ou 'a' e fica igual.");
+        }
+
+    }
+}
